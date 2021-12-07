3827b801191d1ed7293c56a4adec3c48b4a4ea05
ALBATOR <SST.plomberie@laposte.net>
2021-12-07 18:27:40 +0100

    portee , return anonyme function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //ma fonction est une suite d'instructions
 function faireQuelqueChose() {
     console.log("je fais un truc");
+    console.log("Trop cool");
 }
 
 faireQuelqueChose();//j'appelle ma fonction pour la lancer
@@ -16,9 +17,47 @@ const faireUneTache = ()=>{
 
 faireUneTache();
 
-const faireDesTaches = (type)=> {
-    console.log("je fais : " + type);
+const faireDesTaches = (type)=> {     //partout ou j'ai écrit type il va remplacé par la vaisselle
+    console.log("je fais : " + type); //partout ou j'ai écrit type il va remplacé par les courses
 }
 
 faireDesTaches("la vaisselle"); 
 faireDesTaches("les courses"); 
+
+// le return
+function calc(x,y) {
+    return x + y;
+}
+//le return il arrête la fonction et il retourne une valeur!!!!!!!!!
+
+
+
+//une fonction qui se joue toute seule
+
+(function maFonction() {
+    console.log("je me joue toute seule");
+    
+}());//elle se joue à cause des parenthéses à l'intérieur
+
+
+(function () {
+    console.log("je me joue toute seule");
+    
+}());//elle se joue à cause des parenthéses à l'intérieur et en plus elle est anonyme elle n'a pas de nom on pratique ce genre de fonction quand on sait que l'on ne doit plus
+//la rapeler plus tard!!!!!!!
+
+//même principe mais en fléchée
+
+(() => {
+    console.log("je me joue aussi toute seule");
+})();
+
+
+//la portée des variables
+
+function add2() {
+    let a = 4;
+    console.log(a);
+    return a + 2;
+}
+add2();
